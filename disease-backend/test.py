074d7dd3db0de67d69fa542d48847ca0541abf4e
QuinnJL04 <quinnlouie04@gmail.com>
2024-06-19 18:10:23 -0400

    finished flask route

--- disease-backend/test.py
+++ disease-backend/test.py
@@ -14,6 +14,7 @@ import time
 from flask import Flask, jsonify, request
 
 from diabetes import predict_using_all_models
+from heart import predict_using_all_models
 
 from flask_cors import CORS
 app = Flask(__name__)
@@ -24,10 +25,13 @@ def get_current_time():
     now = time.time()
     return jsonify(time='test')
 
-
-
 def transform_gender(gender: str) -> int:
     if gender == 'male':
+        return 1
+    return 0
+
+def transform_sex(sex: str) -> int:
+    if sex == 'male':
         return 1
     return 0
 
@@ -64,6 +68,32 @@ def get_diabetes_result():
     HbA1c = inputs.get('HbA1c')
     blood_glucose = inputs.get('bloodGluclose')
     row = np.array([float(gender), float(age), float(hypertension), float(heart_disease), float(smoking), float(bmi), float(HbA1c), float(blood_glucose)]).reshape(1, -1)
+    result = predict_using_all_models(data=row)
+    return jsonify(result=result)
+
+@app.route('/heart', methods=['POST'])
+def get_heart_result():
+    data = request.get_json()
+    inputs = data.get('formValues')
+    # using inputs get the form values
+    sex = transform_sex(inputs.get('sex'))
+    age = inputs.get('age')
+    cp = inputs.get('cp')
+    trestbps = inputs.get('trestbps')
+    chol = inputs.get('chol')
+    fbs = transform_boolean_to_int(inputs.get('fbs'))
+    restecg = inputs.get('restecg')
+    thalach = inputs.get('thalach')
+    exang = transform_boolean_to_int(inputs.get('exang'))
+    oldpeak = inputs.get('oldpeak')
+    slope = inputs.get('slope')
+    ca = inputs.get('ca')
+    thal = inputs.get('thal')
+    # put values into an array
+    row = np.array([float(sex), float(age), float(cp), float(trestbps), 
+                    float(chol), float(fbs), float(restecg), float(thalach), 
+                    float(exang), float(oldpeak), float(slope), float(ca), 
+                    float(thal)])
     result = predict_using_all_models(data=row)
     return jsonify(result=result)
 
